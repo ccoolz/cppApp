2781f4a88825abe11734dedad363ebc834f1e796
ccoolz <126702055+ccoolz@users.noreply.github.com>
2024-03-31 00:04:45 +0800

    此版本解决了主线程与子线程通信中遇到的死锁问题
    主要更改了 threadFunc函数 和 线程池析构函数，将之前设计上的失误和没有覆盖到的情况进行了改进

---
+++ ThreadPool/ThreadPool/threadpool.h
@@ -0,0 +1,250 @@
+/* 本多线程池库Fixed模式下的基本使用示例 Example -------
+
+ThreadPool pool;		// 创建线程池对象
+pool.start(5);			// 创建你传入数量的线程，开启线程池（默认线程数是4）
+
+class MyTask : public Task	// 你需要创建自己的任务类继承此库的任务基类，然后重写基类的run方法，也就是你要提交的函数的运行方法
+{
+public:
+	void run() { 自定义... }
+}
+
+// 直接运行
+pool.submitTask(std::makeshared<MyTask>())	// 传入你要多线程运行的函数，线程会自动获取并运行
+
+// 如果需要返回值，用Result类接
+Result res = pool.submitTask(std::makeshared<MyTask>())
+// 假如返回值是int类型，调用以下方法
+int val = res.getAny().cast<int>();
+------------------------------------------ */
+
+// #pragma once			// 使头文件不会被重复包含，不过并不是所有编译器都支持，下面写一个通用的
+#ifndef THREADPOOL_H	// 虽然是语言级别的，编译器通用，但是也有编译时间更长的缺点 -- 每次都要打开头文件进行检查
+#define THREADPOOL_H
+#include <iostream>
+#include <vector>
+#include <queue>
+#include <memory>
+#include <atomic>
+#include <mutex>
+#include <condition_variable>
+#include <functional>
+#include <thread>
+#include <unordered_map>
+
+// 任务抽象基类		多态思想: 让线程可以接收不同类型的任务（基类指针），派生类重写基类的run方法（指向派生类对象），实现使用相同方法对不同类型任务的不同处理
+class Result;
+class Any;
+class Task
+{
+public:
+	Task();
+	~Task() = default;
+
+	//exec() :	 1. 执行任务； 2. 通过passValue方法把任务的返回值传递给Result
+	void exec();
+
+	void setResult(Result* res_);
+
+	virtual Any run() = 0;
+private:
+	Result* result;		// 不能用智能指针，因为Result类里有一个Task智能指针成员，智能指针互相指向可能出现交叉引用的死锁问题。而且在这里我们知道result的生命周期 > task对象（task生命只在提交任务的那一行），所以用裸指针就行
+};
+
+// 线程池模式枚举	合理地使用枚举，可以提高代码的可读性和可维护性，使得程序更加清晰易懂
+enum class PoolMode		// 之前的枚举使用枚举型不用加枚举类型，导致可能出现不同枚举里的同名枚举项问题，新标准里可以用 enum class防范这个问题。使用 enum class需要用作用域访问枚举项
+{
+	MODE_FIXED,		// 固定线程数量模式，依机器核数而定		-- 场景：处理耗时的任务
+	MODE_CACHED		// 线程数量可动态增长					-- 场景：处理小而快的任务，任务处理比较紧急
+};
+
+// 线程类
+class Thread
+{
+	using ThreadFunc = std::function<void(int)>;
+public:
+	Thread(ThreadFunc func_);
+
+	~Thread();
+
+	void start();	// 启动线程
+
+	int getId() const;
+private:
+	ThreadFunc func;
+
+	static int generate_id;		// 线程id是多少无所谓，只要不一样可以代表这个线程就行，用这个变量变化产生线程id
+	int thread_id;		// 保存线程id
+};
+
+// 线程池类
+class ThreadPool
+{
+public:
+	ThreadPool();
+
+	~ThreadPool();
+
+	void setMode(PoolMode mode);		// 设置线程池的模式
+
+	void start(int init_thread_size = std::thread::hardware_concurrency());		// 开启线程池，设定默认的初始线程数量为4，用户可以传入参数改变该值
+	// std::thread::hardware_concurrency ：硬件并发 会返回运行该程序的计算机的处理器数量，Windows可以在 计算机管理-设备管理器-处理器 下查看，我的是12个处理器核心
+
+	void setTaskSizeUpperLimit(int max_size);		// 设置任务队列数量上限
+
+	void setThreadSizeUpperLimit(int max_size);		// cached模式下，设置线程队列数量上限
+
+	Result submitTask(std::shared_ptr<Task> task);	// 用户给线程池提交任务
+
+	bool checkRunningState();
+
+	ThreadPool(const ThreadPool& thread_pool) = delete;		// 禁用拷贝构造
+
+	ThreadPool& operator=(const ThreadPool& thread_pool) = delete;		// 禁用赋值
+			// 用户可以创建线程池对象，不过线程池对象涉及了很多内容，锁，容器.. 最好禁用对线程池对象的拷贝行为
+private:
+	void threadFunc(int thread_id);		// 定义线程函数
+	
+private:
+	//std::vector< std::unique_ptr<Thread>> threads;			// 线程数组，存储线程的容器		有指针就要有析构，自己析构很麻烦，完全可以通过智能指针
+	std::unordered_map<int, std::unique_ptr<Thread>> threads;	// 改成键值对的形式，通过id访问线程从而更精确的管理线程
+	int initial_thread_size;				// 初始的线程数量	start()默认初始化为CPU核数，因为start()是使用线程池必须先调用的方法，所以在构造函数中专门初始化该值为0就行
+	std::atomic_int idle_thread_size;		// 记录空闲线程的数量	只要是会被不同线程同时操作的变量，尽量使用原子类型
+	int thread_size_upper_limit;			// 线程数量上限		cached模式下线程数量不能无休止地增加
+	std::atomic_int cur_thread_size;		// 当前线程数量
+
+	std::queue< std::shared_ptr<Task>> task_que;			// 任务队列，为了实现多态，以任务基类的指针或引用初始化。
+				/* 要考虑一种情况: 在submitTask(task) 中，若用户传入的是临时任务对象，则该行语句结束后该对象就被析构
+				线程接收的指针指向被释放的内存，没有意义，所以形参设为Task对象的智能指针 -- 可以延长临时对象的生命周期到run方法结束后，并自动释放其资源 */
+	std::atomic_int task_size;				// 任务的数量			知道一定不是负数就可以用unsigned int，多线程对任务数量的访问的互斥性可以用原子类型实现
+	int task_size_upper_limit;			// 任务数量上限
+	
+	std::mutex task_que_mtx;				// 保证任务队列线程安全的互斥锁
+	std::condition_variable not_full;		// 表示任务队列不满
+	std::condition_variable not_empty;		// 表示任务队列不空
+	std::condition_variable pool_end;		// 线程池析构，等待线程全部回收
+
+	PoolMode pool_mode;						// 线程池的模式
+	std::atomic_bool is_pool_runnig;		// 线程池是否在运行，放在线程池的成员函数里防止用户不开启线程池直接调用方法的误操作
+};
+
+// 自己创建的Any类型，可以接收任意数据的类型 (模板类的代码都只能写在一个头文件中被包含，不能分文件编写声明和定义)
+class Any
+{
+public:
+	Any() = default;
+	~Any() = default;
+	Any(const Any&) = delete;	// 它的成员变量base是用unique_ptr构建的，unique_ptr同一时间只能指向一个对象，所以禁用了左值引用的拷贝构造和赋值操作符，作为包含这个成员变量的Any，当然也要禁用左值引用 -- 左值引用了类型，其中的成员变量自然也一起被左值引用了
+	Any& operator=(const Any&) = delete;
+	Any(Any&&) = default;		// 右值引用默认，那么每个成员的右值引用就继续保持成员自己的右值引用方式
+	Any& operator=(Any&&) = default;	// 实际上，上面四行不写，默认的调用到成员也会报错，只是加强印象和尽量使代码逻辑完美
+
+	// 这个构造函数模板可以让Any接收任意类型的数据，保存在基类Base指针指向的成员类Derived的泛型成员data里
+	template<typename T>
+	Any(T data_)
+		: base( std::make_unique<Derived<T>>(data_))
+	{}
+	/*	在以Any作为返回值类型，接收到一个返回值，就会以该值进行 Any类型的构造，base是Any的成员变量，类型是Base指针，也就是基类指针
+		传入值作为派生类的构造函数的参数，进行派生类对象的构造，派生类是类模板，传入值被赋给派生类的成员 - 泛型的data
+
+	*/
+
+	// 这个方法能把保存的data提取出来
+	template<typename T>  // 用户传入什么类型，就转换成什么类型的意思
+	T cast()
+	{
+		// 我们要通过 Any类的成员base指针找到它指向的派生类对象，从里面取出存储的data变量
+		// 基类指针 转成 派生类的指针，向下转，要想成功，这个基类指针需要确确实实指向一个派生类对象
+		// 四种类型强转方法中，dynamic_cast是支持RTTI类型识别的  ---  RTTI是运行阶段类型识别（Runtime Type Identification）的简称。RTTI旨在为程序再运行阶段确定对象的类型提供一种标准方式。
+		// 如果可能的话，dynamic_cast运算符将使用一个指向基类的指针来生成一个指向派生类的指针；否则，该运算符返回0（空指针）；
+		// 这是最常用的RTTI组件，它不能回答“指针指向的是哪类对象”这样的问题，但能够回答“是否可以安全地将对象的地址赋给特定类型的指针”这样的问题。
+		// 通常，如果指向的对象（*pt）的类型为Type或者是从Type直接或简介派生而来的类型，则下面的表达式将指针pt转换为Type类型的指针：
+		// dynamic_cast<Type*>(pt)	否则，结果为0，即空指针。 即发现不可以转换时返回空指针
+		Derived<T>* p_d = dynamic_cast<Derived<T>*>(base.get());	  // 智能指针的get方法，可以获取智能指针里存储的裸指针。方便这里的类型转换
+		
+		if (p_d == nullptr)		// 为空，说明转换失败，那么用户想要cast的类型与他一开始返回的类型是不匹配的
+		{
+			throw "Err: The type you want to cast does not match the return type of the task you submitted!";	// Bug: 这里没加;........
+		}																										// 与模板有关的模块的错误没有具体提示，需要写的更加细致
+		// 若成功转换，返回派生类的data
+		return p_d->data;
+	}
+
+private:
+	// 基类
+	class Base
+	{
+	public:
+		virtual ~Base() = default;		// 差不多是 !Base() {} 也就是函数体为空，不过在新版里编译器可以对default语法做更多优化
+	};
+
+	// 派生类		用模板类，泛型的data包含在类里
+	template< typename T>
+	class Derived : public Base
+	{
+	public:                                      
+		Derived(T data_)
+			: data(data_)
+		{}
+	
+		T data;
+	};
+
+private:
+	// 基类指针
+	std::unique_ptr<Base> base;
+};
+
+// 通过互斥锁和条件变量自己实现一个信号量类  --  为了满足主线程中使用 Result类型的.get方法阻塞到子线程结束再获取值的这个时序关系而设计
+class Semaphore
+{
+public:
+	Semaphore(int sem_num_ = 0)
+		: sem_num(sem_num_)
+	{}
+
+	~Semaphore() = default;
+
+	// 获取一个信号量资源
+	void wait()
+	{
+		std::unique_lock<std::mutex> lock(mtx);
+		// 等待信号量有资源这个条件，没资源则阻塞
+		condition.wait(lock, [&]()->bool { return sem_num > 0; });
+		sem_num--;
+	}
+
+	// 增加一个信号量资源
+	void post()
+	{
+		std::unique_lock<std::mutex> lock(mtx);
+		sem_num++;
+		condition.notify_all();
+	}
+
+private:
+	std::mutex mtx;
+	std::condition_variable condition;
+	int sem_num;							// 因为信号量不像锁是单一资源，它可以有多个，所以需要资源计数变量来辅助实现
+};
+
+// 实现接收用户提交任务完成后的返回值类型Result
+class Result
+{
+public:
+	Result(std::shared_ptr<Task> task_, bool is_valid_ = true);
+	~Result() = default;
+
+	void passValue(Any any_);
+
+	Any getAny();	// 用户调用这个方法获得提交任务完成后的返回值
+
+private:
+	Any any;		// 存储任务的返回值
+	Semaphore sem;	// 线程通信信号量		-- 在分离线程没返回之前，需要阻塞住，既然是两个不同线程，当然需要线程通信，而且仅仅满足一前一后的效果即可，最合适的就是用信号量
+	std::shared_ptr<Task> task;		// 指向对应获取返回值的任务对象
+	std::atomic_bool is_valid;		// 返回值是否有效 -- 若提交任务失败了，则无效。有效才对返回值过程进行阻塞
+};
+
+#endif
+
---
+++ ThreadPool/ThreadPool2_ResolveDeadLock/ThreadPool2_resolveDeadLock/main.cpp
@@ -0,0 +1,137 @@
+#include "threadpool.h"
+
+typedef unsigned long long uLong;
+
+void Hello()
+{
+	std::cout << "Hello ";
+}
+
+class TaskHello : public Task
+{
+public:
+	TaskHello() = default;
+
+	TaskHello(int begin_, int end_)
+		: begin(begin_), end(end_)
+	{}
+
+	// 问题一：怎么设计run的返回值，可以接收任意的类型呢？用模板是不行的，run是虚函数，编译器就要确定虚函数表，而模板是运行时确定类型并替换。
+	// C++17 的Any类型就可以。那么我们自己写个Any试试
+	Any run()
+	{
+		std::cout << "tId: " << std::this_thread::get_id() << " 开始执行\n";
+		Hello();
+		std::cout << "thread " << std::this_thread::get_id() << "\n";
+		uLong sum = 0;
+		for (int i = begin; i <= end; i++)
+			sum += i;
+
+		std::this_thread::sleep_for(std::chrono::seconds(3));
+		std::cout << "tId: " << std::this_thread::get_id() << " 执行结束\n";
+		return sum;		// 返回值sum 传给类型Any 相当于查看Any类是否有一个sum类型参数的构造函数，若有，则以该参数构造
+	}					// 而Any的构造函数以函数模板设计 template< typename T> \Any(T data_) ... 所以无论返回什么类型，Any都可以接收
+private:
+	int begin = 0;
+	int end = 0;
+};
+
+void test1()
+{
+	ThreadPool thread_pool;
+	thread_pool.start(4);
+
+	// Master - Slave 线程模型
+	// Master线程用来分解任务，然后给各个slave线程分配任务
+	// 等待各个slave线程执行完任务，返回结果
+	// Master线程合并各个任务结果，输出
+	Result res1 = thread_pool.submitTask(std::make_shared<TaskHello>(0, 100000000));
+	Result res2 = thread_pool.submitTask(std::make_shared<TaskHello>(100000001, 200000000));
+	Result res3 = thread_pool.submitTask(std::make_shared<TaskHello>(200000001, 300000000));
+	// 如何设计这里的Result机制呢  -- 用return task->Result 这样的设计还是 return Result(task)，答案是后者，因为Task对象的生命周期在上面这一行就结束了，需要用后面的设计来延长到下面一行结束,具体的延长方式就是通过shared_ptr增加其引用计数，所以在Result类中设计了一个shared_ptr对象
+	uLong sum1 = res1.getAny().cast<uLong>();		// !Bug: 未经处理的异常: Microsoft C++ 异常: char，位于内存位置 0x0133FAD  主要原因是这里cast<int>里忘了改成cast<uLong>
+	uLong sum2 = res2.getAny().cast<uLong>();
+	uLong sum3 = res3.getAny().cast<uLong>();
+	std::cout << "sum = " << sum1 + sum2 + sum3 << "\n";
+
+	//thread_pool.submitTask(std::make_shared<TaskHello>());		// 可以直接传任务的匿名对象
+
+	//std::shared_ptr<TaskHello> HelloTask = std::make_shared<TaskHello>();
+	//thread_pool.submitTask(HelloTask);							// 也可以传入实现创建好的任务对象
+	//thread_pool.submitTask(HelloTask);
+	//thread_pool.submitTask(HelloTask);
+	//thread_pool.submitTask(HelloTask);
+	//thread_pool.submitTask(HelloTask);
+	//thread_pool.submitTask(HelloTask);
+	//thread_pool.submitTask(HelloTask);		// 8个任务提交，两条进入队列被两个线程取走，又两个进入被另外两个取走，又两个进入因没有空闲线程
+	//thread_pool.submitTask(HelloTask);
+
+	// 因为线程是detach的，所以主程序运行完，线程资源自动回收，线程函数还没来得及运行完，这不是设计的问题，实际应用中，这个服务就是持续运行的，加上休眠时间测试一下
+	// std::this_thread::sleep_for(std::chrono::seconds(5));
+	getchar();
+}
+
+void test2()
+{
+	ThreadPool thread_pool;
+	thread_pool.start(4);
+	thread_pool.setMode(PoolMode::MODE_CACHED);
+
+	thread_pool.submitTask(std::make_shared<TaskHello>(0, 100000000));
+	thread_pool.submitTask(std::make_shared<TaskHello>(0, 100000000));
+	thread_pool.submitTask(std::make_shared<TaskHello>(0, 100000000));
+	thread_pool.submitTask(std::make_shared<TaskHello>(0, 100000000));
+	// 初始4条线程，将上面四个任务取走，任务中有延时，不会很快执行完。那么MODE_CACHED下我们预期会创建两条新线程来接下面两个任务
+	thread_pool.submitTask(std::make_shared<TaskHello>(0, 100000000));
+	thread_pool.submitTask(std::make_shared<TaskHello>(0, 100000000));
+
+	getchar();
+}
+
+void test3()
+{
+	ThreadPool thread_pool;
+	thread_pool.start();
+	thread_pool.setMode(PoolMode::MODE_CACHED);
+
+	thread_pool.submitTask(std::make_shared<TaskHello>(0, 100000000));
+	thread_pool.submitTask(std::make_shared<TaskHello>(0, 100000000));
+	thread_pool.submitTask(std::make_shared<TaskHello>(0, 100000000));
+	thread_pool.submitTask(std::make_shared<TaskHello>(0, 100000000));
+
+	thread_pool.submitTask(std::make_shared<TaskHello>(0, 100000000));
+	thread_pool.submitTask(std::make_shared<TaskHello>(0, 100000000));
+	thread_pool.submitTask(std::make_shared<TaskHello>(0, 100000000));
+	thread_pool.submitTask(std::make_shared<TaskHello>(0, 100000000));
+
+	thread_pool.submitTask(std::make_shared<TaskHello>(0, 100000000));
+	thread_pool.submitTask(std::make_shared<TaskHello>(0, 100000000));
+	thread_pool.submitTask(std::make_shared<TaskHello>(0, 100000000));
+	thread_pool.submitTask(std::make_shared<TaskHello>(0, 100000000));
+	// 加上下面的任务，共13个任务。
+	// 测试 std::thread::hardware_concurrency()返回的处理器数量是否正确(12个)，若正确，运行test3会创建一条新线程来处理第13个任务
+	thread_pool.submitTask(std::make_shared<TaskHello>(0, 100000000));
+
+	getchar();
+}
+
+void test4()
+{
+	// 项目中遇到的死锁问题			-- 产生原因见 ThreadFunc()中的 while处
+	int i = 10;
+	while (i--)
+	{
+		ThreadPool thread_pool;
+		thread_pool.start(4);
+
+		Result res = thread_pool.submitTask(std::make_shared<TaskHello>(0, 100000000));
+		uLong sum = res.getAny().cast<uLong>();
+		std::cout << "sum = " << sum << std::endl;
+	}
+	std::cout << "MAIN END\n";
+}
+
+int main()
+{
+	test4();
+}
